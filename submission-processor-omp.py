0135b9df4ec7da6c6a77cc1f69cf969dbf0c826a
forbes1799 <123934016+forbes1799@users.noreply.github.com>
2023-05-30 12:04:47 +0100

    Update submission-processor-omp.py

--- submission-processor-omp.py
+++ submission-processor-omp.py
@@ -41,11 +41,12 @@ def submit_job_for_run(exe, threads, identifier, artifacts_path, basedir):
     
     threadValue = None
     resultsDone = False    
-
+    print("threads", threads)
     with open(results_file_name, 'r') as file:
         results = csv.DictReader(file)
         for row in results:
             threadValue = row['num_threads']
+	    print("threadValue", threadValue)
             if threadValue == threads:
                 resultsDone = True
 
